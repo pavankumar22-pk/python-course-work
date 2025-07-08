d673c0eab82d2d66a3a8484cb6df2b66fe66ef37
pavankumar22-pk <pavang9491@gmail.com>
2025-07-08 11:37:11 +0530

    Add files via upload

---
+++ d3.py
@@ -0,0 +1,53 @@
+a=123
+b=11.6
+type(b)
+<class 'float'>
+type(a)
+<class 'int'>
+a=4+5j
+type(a)
+<class 'complex'>
+a="kavya"
+type(a)
+<class 'str'>
+s="kavya"
+r=6122
+sb={'maths','science'}
+m=[22,23]
+g=(75.0)
+type(s)
+<class 'str'>
+type(r)
+<class 'int'>
+type(sb)
+<class 'set'>
+type(m)
+<class 'list'>
+type(g)
+<class 'float'>
+t=()
+type(t)
+<class 'tuple'>
+t=(1,2,3,4)
+type(t)
+<class 'tuple'>
+t
+(1, 2, 3, 4)
+k=('kavya','pavan')
+type(k)
+<class 'tuple'>
+k
+('kavya', 'pavan')
+k={}
+type(k)
+<class 'dict'>
+s={1,2,3,4}
+type(s)
+<class 'set'>
+s={1,1,2,3,3,3,4,4,5,5,7,7,9}
+s
+{1, 2, 3, 4, 5, 7, 9}
+a=7
+b=2
+print(a-b)
+
