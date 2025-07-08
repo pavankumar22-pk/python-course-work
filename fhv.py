2412f37ce332eed50b2229a670b3baf7e283dfe6
pavankumar22-pk <pavang9491@gmail.com>
2025-07-08 11:52:38 +0530

    Add files via upload

---
+++ fhv.py
@@ -0,0 +1,36 @@
+helloworld ="abhi"
+print(helloworld)
+name="john"
+print (name)
+a="helloworld"
+print(a)
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+product_name="lappy"
+rate=47500
+availability=False
+print(product_name,rate,availability)
+material="car"
+model=2025
+brand= "benz" 
+attributes="fastmoving"
+print(material,model,brand,attributes)
+a=6
+b=7
+c=9
+print(a,c)
+a=b=c=d=22
+print(a,d)
+x=7
+x=8
+x=6
+print(x)
+a="pavan"
+b="kavya"
+del a
+print(b)
+a="kavya"
+t=("maths,science")
+l=["10,20"]
+type(t)
