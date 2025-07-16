f082d70bc79bc5d2fb1ada38fad17ab54463a0f0
pavankumar22-pk <pavang9491@gmail.com>
2025-07-16 10:17:53 +0530

    Add files via upload

---
+++ d4.py
@@ -0,0 +1,198 @@
+# 1.Arithmetic operators
+a=15
+b=30
+print("Addition:",a+b)#Addition: 45
+print("Subtraction:",a-b)#Subtraction: -15
+print("Multiplication:",a*b)#Multiplication: 450
+print("Division:",a/b)#Division: 0.5
+print("Float division:",a//b)#Float division: 0
+#modulus
+a=6
+b=5
+print("Modulus:",a%b)#Modulus: 1
+print("Exp:",a**b)#Exp: 7776
+a=16
+b=4
+print(a+b)
+print(a%b)
+a='abhii'
+b='pichi'
+(a==b)
+# 2.comparision operators
+a=30
+b=40
+print("Equal to:",a==b)#Equal to: False
+print("Not equal to:",a!=b)#Not equal to: True
+print("Greater than:",a>b)#Greater than: False
+print("Less than:",a<b)#Less than: True
+print("Greater than or equal to:",a>=b)#Greater than or equal to: False
+print("Less than or equal to:",a<=b)#Greater than or equal to: False
+#comparision operator
+x='kavya'
+y='kavya'
+print(x!=y) #False
+# 3.assignment operators
+a=30
+a+=50
+print("Add & assign:",a)
+print("Sutract and assign:",a)
+print("Multiply and assign:",a)
+print("Divide and assign:",a)
+print("Modulus and assign:",a)
+print("Exponentite and assign:",)
+#as operators
+a=20
+a+=30
+print("Add & assign:",a) #Add & assign: 50
+a-=40
+print("Sutract and assign:",a) # Sutract and assign: 10
+a*=60
+print("Multiply and assign:",a)#Multiply and assign: 600
+a/=700
+print("Divide and assign:",a)#Divide and assign: 0.8571428571428571
+a%=20
+print("Modulus and assign:",a)#Modulus and assign: 0.8571428571428571
+a*=5
+print("Exponentite and assign:",)
+# 4.logical operators
+#membership operators
+t=("redddy","nagu","raavi")
+"pavan" in t
+False
+"nagu" in t
+True
+me="pavan"
+"k" in me
+False
+"a" in me
+True
+"n" not in me
+False
+names=['pavan','chinni','munni']
+print('in result:','kavya' in names) #in result: False
+print('not in result:','pavan' not in names) #not in result: False
+#identify operators
+l=[1,2,3,4]
+b=[1,2,3,4]
+print("l is b:",l is b)#l is b:False
+a=b #assign b to a
+print("a is b:",a is b)#a is b:True
+print("id(1)",id(l))#id(l) 2608353285312
+print("id(b)",id(b))# id(b) 2608308008064
+print("id(a)",id(a)) #id(a) 2608308008064
+print("a is not b:",a is not b)#a is not b:False
+print("l is not b:",l is not b) #l is not b:False
+#membership operator
+a='kavya'
+'v' not in a
+False
+k='pavan'
+'k' in k
+False
+names=["kavudu","bandodu","sravudu"]
+'naidu' in names
+False
+'a'in k
+True
+s={1,2,3,4,5,6}
+2 in s
+True
+7 in s
+False
+t=(1,2,3)
+4 in t
+False
+2 in t
+True
+'pav' in k
+True
+data={'name':'kavya','batch':'aiml','dob':1-2-2003}
+'dob' in data
+True
+'rs status' in data
+False
+'aiml' in data
+False
+'kavya' in data
+False
+'batch' in data
+True
+#bitwise operators
+1-0o001
+2-0o010
+3-0o011
+4-100
+5-101
+6-110
+7-111
+
+3-0o011
+5-101
+------
+3&5=0o001=1'''
+print("3&5:",3&5)#3&5:1
+
+'''4-100
+5-101
+------
+4|5=101=5'''
+print("4|5:",4|5)#4|5:5
+
+''' 5-101
+6-110
+------
+5^6=0o011=3'''
+print("5^6:",5^6)#%^6:3
+'''1-101
+-------
+~1=110'''
+print("~1:",~1)
+'''2<<1
+2=0o010
+4=100'''
+print("2<<1:",2<<1)#2<<1:4
+'''4>>1
+4=100
+2=0o010'''
+print ("4>>1:",4>>1) #4>>1:2
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
+
